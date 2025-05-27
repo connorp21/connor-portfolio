83eaf94961b84556001aafbeb9014f25298132bd
Connor Parker <cmccordp01@gmail.com>
2025-05-27 13:36:34 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/connorp21.github.io',
+  base: '/connor-portfolio',
   plugins: [react()],
 })
